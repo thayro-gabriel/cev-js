b4193c442746fd2482a441a387715ddb11a8a37c
Watari151 <bielfsa2016@gmail.com>
2024-05-23 20:03:02 -0300

    DÁ-LHE ISSO TÁ COMEÇANDO A FICAR DIVERTIDOOOOOOOO

---
+++ ex012.js
@@ -0,0 +1,7 @@
+var velo = 60.5
+console.log(`A velocidade do seu carro é ${velo}km/h`)
+if (velo >= 80) {
+    console.log(`VOCÊ ULTRAPASSOU A VELOCIDADE LIMITE DA ESTRADA, MULTADO!!!`)
+} else {
+    console.log(`${velo}km/h é suave paizão. Dá-lhe. Mete marcha!`)
+}
