e41aeab5668e5cf80acb1953f02bb3a4072966a2
Watari151 <bielfsa2016@gmail.com>
2024-05-24 21:31:30 -0300

    praticando todo dia

---
+++ ex015.js
@@ -0,0 +1,11 @@
+let idade = 12
+console.log(`voce tem ${idade} anos`)
+if (idade >= 65) {
+    console.log('seu voto é opcional, velho gagá')
+} else if (idade > 18) {
+    console.log('tem que votar papitooo')
+} else if (idade >= 16) {
+    console.log('voto opcional papitooooooo')
+} else {
+    console.log("simplesmente não vote, criaturinha imunda.")
+}
---
+++ ex017.js
@@ -0,0 +1,28 @@
+var agora = new Date()
+var hoje = agora.getDay()
+switch (hoje) {
+    case 0:
+        console.log('Hoje é domingo... À noite...')
+        break
+    case 1:
+        console.log('Hoje é segunda, começou o pagode.')
+        break
+    case 2:
+        console.log('Terça, jogue duro!')
+        break
+    case 3:
+        console.log('Tamo na quarta, meio caminho andado!')
+        break
+    case 4:
+        console.log('on my to the hell, QUINTA AAAAAAAA')
+        break
+    case 5:
+        console.log('LAST FRIDAY NIGHT TCHURURURURURUURURURU')
+        break
+    case 6:
+        console.log('O MELIOR DEA DA SEMANAAAA')
+        break
+    default:
+        console.log('wtf???')
+        breal
+}
