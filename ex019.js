363453d123796a3686bfb032c12a2dedee4d0fb8
Watari151 <bielfsa2016@gmail.com>
2024-05-26 15:29:10 -0300

    daily commit, whatever

---
+++ ex019.js
@@ -0,0 +1,10 @@
+var contador = 1
+/*while (contador <= 500) {
+    console.log(`Passo ${contador}`)
+    contador++
+}*/
+
+do {
+    console.log(`Vingadores ${contador}`)
+    contador++
+} while (contador <= 500)
---
+++ ex020.js
@@ -0,0 +1,6 @@
+console.log('Começando papai')
+var c = 1
+for (c=1;c<=10;c++) {
+    console.log(`OMAIGAAAAAAA ${c}`)
+}
+console.log('FInalizando papai')
